d366a89ddb563c834357f6c0b9d5eb5fb9375755
sachinthawijesundara666 <sachintha.20250307@iit.ac.lk>
2026-08-20 13:19:06 +0530

    Using Path objects to gather Files

---
+++ OrganizerLogic/Scanner.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+SKIP_FOLDERS = {"Organized"}
+
+def scan_folder(source: Path) -> list[Path]:
+    files = []
+
+    for item in source.iterdir():
+        if item.is_file():
+            files.append(item)
+        elif item.is_dir() and item.name in SKIP_FOLDERS:
+            continue 
+
+    return files
