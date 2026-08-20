6a09e8f64b206641bf29f3b0c5fec3b26fe25c25
sachinthawijesundara666 <sachintha.20250307@iit.ac.lk>
2026-08-20 12:18:30 +0530

    Created a File Selection Window

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("Hello World!")
-print ("HI")
+from pathlib import Path
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+
+#Using Tkinter to open a Folder Selection window
+root = tk.Tk()
+root.withdraw()
+
+source = filedialog.askdirectory(title="Select a Folder To ORganize")
+
+if not source:
+    messagebox.showerror("Error", "No file selected", parent=root)
+else:
+    source = Path(source)
+
+root.destroy()
